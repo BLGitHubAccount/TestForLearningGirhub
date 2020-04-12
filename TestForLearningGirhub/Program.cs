181b71561102cb452838c94fbca56bb77ec4a712
baoliangjin <mrbaoliangjin@gmail.com>
2020-04-12 11:08:38 +0800

    my second commit

--- TestForLearningGirhub/Program.cs
+++ TestForLearningGirhub/Program.cs
@@ -7,6 +7,10 @@ namespace TestForLearningGirhub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
